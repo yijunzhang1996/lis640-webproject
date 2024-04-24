4b5af687763f3845554a14ec43ebd6857a952f5a
yijunzhang1996 <276402688@qq.com>
2024-04-24 11:44:44 -0500

    edit the homepage

---
+++ homepage.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel-image");
+  let indicators = document.getElementsByClassName("indicator");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";
+  }
+  for (i = 0; i < indicators.length; i++) {
+    indicators[i].className = indicators[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.opacity = "1";
+  indicators[slideIndex-1].className += " active";
+}
+
+document.querySelector('.prev').addEventListener('click', function() {
+  plusSlides(-1);
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+  plusSlides(1);
+});
